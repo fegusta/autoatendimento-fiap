1a2ef2a4c1737bfba27e87de5f493c34e4aa0a6c
fegusta <fegtnc@gmail.com>
2025-05-17 10:36:51 -0300

    interface produto

---
+++ app/ports/produto_repository.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+from typing import List, Optional
+from domain.produto import Produto, CategoriaProduto
+
+
+class ProdutoRepository(ABC):
+
+    @abstractmethod
+    async def criar(self, produto: Produto) -> Produto:
+        pass
+
+    @abstractmethod
+    async def atualizar(self, produto: Produto) -> Produto:
+        pass
+
+    @abstractmethod
+    async def remover(self, produto_id: UUID) -> None:
+        pass
+
+    @abstractmethod
+    async def buscar_por_id(self, produto_id: UUID) -> Optional[Produto]:
+        pass
+
+    @abstractmethod
+    async def listar_por_categoria(self, categoria: CategoriaProduto) -> List[Produto]:
+        pass
